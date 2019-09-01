ba48a8421497c088f4d94ca8d027067eaaa78255
Armel <armelfli50@gmail.com>
2019-09-01 04:11:15 -0500

    Added some styling and refactored the code

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,105 +6,113 @@ require 'pry'
 
 
 class CommandLineInterface 
-attr_accessor :url
+  attr_accessor :url
 
-  def initialize 
-   welcome
-   proccess
-  end
-
-  def welcome 
-    puts "--------------------------------------------------"
-    puts "|      Welcome To Our Wonderful CLI              |"
-    puts "|                                                |"
-    puts "--------------------------------------------------"
-    puts ""
-    puts ""
-  end 
-
-  def zip_code? 
-    puts "Do you want to know the cheapest gas stations near you?"
-  end 
-  
-
-
-  def proccess  
-    zip_code? 
-    answer_to_zip_code = gets.strip 
-    if ["Yes","Y"].include?(answer_to_zip_code.upcase) 
-      puts " yayyy! You can now enter your zip code"
-      the_user_zip_code = gets.strip
-      scraper = Scraper.new(the_user_zip_code)
-      
-      url ="https://www.gasbuddy.com/home?search=#{scraper.zip_code}&fuel=1" 
-      gas_stations = scraper.class.sraper_the_cheapest_nearby(url)
-     
-   
-      Gas_station.create_gas_station_from_data(gas_stations)
-      
-     
-      display
-      
-      
-      puts "Do you want detailled Information? (yes/no)" 
-      more_info_answer = gets.strip 
-
-      ["YES","Y"].include?(more_info_answer.upcase) ? more_info : nil
-
-      
-    else 
-      puts "Goodbye!!!!"
+    def initialize 
+    welcome
+    proccess
     end
-  end
 
-  def valid?(zip_code)
-    true if zip_code.to_i != 0
-  end
 
-  def display
-    # puts "#{@search}".colorize(:blue) 
-    
-    Gas_station.all.each do |gas_station|
-      puts "=====================================================================================".colorize(:blue)
-      puts "Gas Station: ".colorize(:green) + "#{gas_station.name}".colorize(:yellow)
-      puts "price: ".colorize(:green) + "#{gas_station.price}".colorize(:yellow)
-      puts "Gas Address: ".colorize(:green) + "#{gas_station.address}".colorize(:yellow)
-      puts "=====================================================================================".colorize(:blue)
-      puts "" 
+    def welcome 
+      puts "--------------------------------------------------".colorize(:light_blue)
+      puts "|                                                |".colorize(:light_blue)
+      puts "|         Welcome To Our Wonderful CLI           |".colorize(:light_blue)
+      puts "|                                                |".colorize(:light_blue)
+      puts "--------------------------------------------------".colorize(:light_blue)
+      puts ""
       puts ""
     end 
 
-    
-  end 
 
-
-  def more_info 
-    Gas_station.all.each do |gas_station| 
-      new_info = Scraper.scraper_detailled_info("https://www.gasbuddy.com#{gas_station.detail_url}") 
+    def proccess  
+      puts "Would like to see the cheapest gas stations near you? (yes/no)"
+      answer_to_zip_code = gets.strip 
      
-      gas_station.add_more_info(new_info)
-    end
-    display_more_info 
-    
-  end
+      ["YES","Y"].include?(answer_to_zip_code.upcase) ? basic_info : goodbye
 
-  def display_more_info 
-    
-    Gas_station.all.each do |gas_station|
-      puts "=====================================================================================".colorize(:blue)
-      puts "Gas Station: ".colorize(:green) + "#{gas_station.name}".colorize(:yellow)
-      puts "Overal Price: ".colorize(:green) + "#{gas_station.price}".colorize(:yellow)
-      gas_station.new_array.each do |el|
-        puts "        Type of Gas: " + "#{el[:type_of_gas]}"
-        puts "            Price of Gas: " + "#{el[:price_of_gas]}"
-        puts "             Reported: " + "#{el[:las_report]}"
-      end 
-      puts  "Overall Rating: " + "#{gas_station.rating}"
-      puts "Gas Address: ".colorize(:green) + "#{gas_station.address}".colorize(:yellow)
-      puts "=====================================================================================".colorize(:blue)
-      puts "" 
+        puts "Would you like more information? (yes/no)" 
+        more_info_answer = gets.strip 
+
+      ["YES","Y"].include?(more_info_answer.upcase) ? more_info : goodbye
+    end
+
+
+    def valid?(zip_code)
+      true if zip_code.to_i != 0 && zip_code.length == 5
+    end
+
+
+    def goodbye 
+      puts "--------------------------------------------------".colorize(:light_blue)
+      puts "|                                                |".colorize(:light_blue)
+      puts "|               Goodbye!!!!!                     |".colorize(:light_blue)
+      puts "|                                                |".colorize(:light_blue)
+      puts "--------------------------------------------------".colorize(:light_blue)
+      puts ""
       puts ""
     end
-  end
-end 
+    
 
+    def display
+      Gas_station.all.each do |gas_station|
+        puts "=====================================================================================".colorize(:blue)
+        puts "Gas Station: ".colorize(:green) + "#{gas_station.name}".colorize(:yellow)
+        puts "price: ".colorize(:green) + "#{gas_station.price}".colorize(:yellow)
+        puts "Gas Address: ".colorize(:green) + "#{gas_station.address}".colorize(:yellow)
+        puts "=====================================================================================".colorize(:blue)
+        puts "" 
+        puts ""
+        puts ""
+      end 
+    end 
+    
+
+    def basic_info 
+      puts "You can now enter your zip code."
+      the_user_zip_code = gets.strip
+      while !valid?(the_user_zip_code) do 
+        puts "Please enter a valid zip code"
+        the_user_zip_code = gets.strip
+      end
+      url ="https://www.gasbuddy.com/home?search=#{the_user_zip_code}&fuel=1" 
+      gas_stations = Scraper.scraper_the_cheapest_nearby(url)
+      Gas_station.create_gas_station_from_data(gas_stations)
+    
+      display
+    end
+
+
+    def more_info 
+      Gas_station.all.each do |gas_station| 
+        new_info = Scraper.scraper_detailled_info("https://www.gasbuddy.com#{gas_station.detail_url}") 
+        gas_station.add_more_info(new_info)
+      end
+      display_more_info 
+    end
+
+
+    def display_more_info 
+      Gas_station.all.each do |gas_station|
+        puts "=====================================================================================".colorize(:blue)
+        puts "Gas Station: ".colorize(:green) + "#{gas_station.name}".colorize(:yellow)
+        puts "Price: ".colorize(:green) + "#{gas_station.price}".colorize(:yellow)
+
+        gas_station.new_array.each do |el|
+          puts "--------------------------------------------------------------------------------------"
+          puts "        Type of Gas: ".colorize(:green) + "#{el[:type_of_gas]}".colorize(:yellow)
+          puts "       Price of Gas: ".colorize(:green) + "#{el[:price_of_gas]}".colorize(:yellow)
+          puts "       Last Reported: ".colorize(:green) + "#{el[:last_report]}".colorize(:yellow)
+          puts "--------------------------------------------------------------------------------------"
+        end
+
+        puts  "Overall Rating: ".colorize(:green) + "#{gas_station.rating}".colorize(:yellow)
+        puts "Gas Address: ".colorize(:green) + "#{gas_station.address}".colorize(:yellow)
+        puts "=====================================================================================".colorize(:blue)
+        puts "" 
+        puts ""
+        puts ""
+      end
+    end
+
+end 
--- lib/gas_station.rb
+++ lib/gas_station.rb
@@ -7,27 +7,24 @@ class Gas_station
         @address = address 
         @price = price 
         @detail_url = detail_url
-        
-    end
+    end 
+
 
     def self.create_gas_station_from_data(array_of_gas_station) 
+
         array_of_gas_station.each do |gas_station|
              gas_object =self.new(gas_station) 
              @@all << gas_object if gas_object.price
         end
-
     end 
+
 
     def add_more_info(data)
         the_gas_station = self
-        data.each do |key,val|
-            # puts "#{key} #{val}"
-            
-             the_gas_station.send("#{key}=",val) 
-        end
-
+        data.each {|key,val| the_gas_station.send("#{key}=",val) }
     end
   
+    
     def self.all 
         @@all.sort_by{|gas_station| gas_station.price }
     end
